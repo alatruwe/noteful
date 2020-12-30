e4e60df25193d52e14fcdd42639f9b4177f25753
adelinel <adeline.latruwe@gmail.com>
2020-12-30 14:23:44 -0800

    added context to Note component

--- src/composition/Note/Note.js
+++ src/composition/Note/Note.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ApiContext from "../../ApiContext.js";
 import "./Note.css";
 
-export default function Note(props) {
-  return (
-    <div className="Note">
-      <h2 className="Note__title">
-        <Link to={`/note/${props.id}`}>{props.name}</Link>
-      </h2>
-      <div className="Note__dates">
-        <div className="Note__dates-modified">
-          Modified <span className="Date">{props.modified}</span>
+export default class Note extends React.Component {
+  static contextType = ApiContext;
+
+  render() {
+    const { id, name, modified } = this.context;
+    return (
+      <div className="Note">
+        <h2 className="Note__title">
+          <Link to={`/note/${id}`}>{name}</Link>
+        </h2>
+        <div className="Note__dates">
+          <div className="Note__dates-modified">
+            Modified <span className="Date">{modified}</span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
