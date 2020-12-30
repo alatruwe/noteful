820f3fe62213022e97c44a8b7034627c581ab00d
adelinel <adeline.latruwe@gmail.com>
2020-12-30 14:18:14 -0800

    added context to NoteListMain

--- src/composition/NoteListMain/NoteListMain.js
+++ src/composition/NoteListMain/NoteListMain.js
@@ -1,22 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Note from "../Note/Note.js";
+import ApiContext from "../../ApiContext.js";
 import "./NoteListMain.css";
 
-export default function NoteListMain(props) {
-  return (
-    <section className="NoteListMain">
-      <ul>
-        {props.notes.map((note) => (
-          <li key={note.id}>
-            <Note id={note.id} name={note.name} modified={note.modified} />
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
-}
+export default class NoteListMain extends React.Component {
+  static defaultProps = {
+    match: {
+      params: {},
+    },
+  };
+  static contextType = ApiContext;
 
-NoteListMain.defaultProps = {
-  notes: [],
-};
+  render() {
+    const { folderId } = this.props.match.params;
+    const { notes = [] } = this.context;
+    const getNotesForFolder = (notes = [], folderId) =>
+      !folderId ? notes : notes.filter((note) => note.folderId === folderId);
+    const notesForFolder = getNotesForFolder(notes, folderId);
+
+    return (
+      <section className="NoteListMain">
+        <ul>
+          {notesForFolder.map((note) => (
+            <li key={note.id}>
+              <Note id={note.id} name={note.name} modified={note.modified} />
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+}
